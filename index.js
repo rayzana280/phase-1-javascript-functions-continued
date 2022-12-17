73cc5f97f57336864ea0cacc7e158df9bf2bad23
Ray Zana <rayzana280@gmail.com>
2022-12-17 00:53:16 -0500

    commit tree 304a13a5433125981ce8e8525906693403d0cbc6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective(light='*'){
+    return function(wish = 'special'){
+        return `You are ${light}${wish}${light}!`;
+    }
+}
